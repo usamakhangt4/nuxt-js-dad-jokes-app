f809e534776b065f7222d96ccb842b63700a6517
Muhammad Usama Bin Akhtar Khan <usamakhangt4@gmail.com>
2020-08-23 01:20:57 +0500

    fixes

---
+++ pages/about.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1 class="text-6xl">About Dadjokes</h1>
+    <p>An app about corny dadjokes for you ...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "About Dad Jokes",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "best place for corny dad jokes"
+        }
+      ]
+    };
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <div>
+      home
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Dad Jokes",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "best place for corny dad jokes"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+</style>
